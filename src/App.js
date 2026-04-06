b06f1f2f42bd6c99f904be8bdca609c9c596895b
NeilMathew33 <mathew.neil33@gmail.com>
2026-04-05 21:17:00 -0700

    Did new photo gallery

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AddPhotoEntry from './Components/AddPhotoEntry';
+import ViewAllPhotos from './Components/ViewAllPhotos';
+import PhotoNavbar from './Components/PhotoNavbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element= {<AddPhotoEntry />}/>
+      <Route path="/ViewAll" element= {<ViewAllPhotos />}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
